a663437173e7edba00c2572f49b017a3ee8aee66
Jonathan Molero <110490630+molerojj@users.noreply.github.com>
2023-05-08 19:42:34 -0400

    project pets

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,5 +1,6 @@
 import { DiCss3, DiHtml5, DiJavascript, DiReact } from './SkillsData'
 import rick from '../Images/rym.png'
+import pets from '../Images/pets.png'
 
 export const projectsData = [
   {
@@ -11,6 +12,17 @@ export const projectsData = [
     github: 'https://github.com/molerojj/rick-and-morty-app-react',
     technologies: [<DiHtml5 />, <DiCss3 />, <DiJavascript />, <DiReact />],
     description: 'Aplicación que muestra cards de los personajes de Rick & Morty.',
+    newProject: true
+  },
+  {
+    id: 0,
+    type: 'frontend',
+    title: 'PetsAmerica',
+    image: `${pets}`,
+    deploy: 'https://pf-henry-one.vercel.app/',
+    github: 'https://github.com/erikfille/PF-Henry',
+    technologies: [<DiHtml5 />, <DiCss3 />, <DiJavascript />, <DiReact />],
+    description: 'E-commerce de productos para mascotas.',
     newProject: true
   }
 ]
